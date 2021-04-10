a5999693bc8899d686274bec74cbc5cd0163dc59
EC2 Default User <ee111fender@gmail.com>
2021-04-10 12:38:26 +0000

    [Update]アプリ開発

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+    root to: 'homes#top'
     devise_for :users
     resources :users, only:[:index, :show, :edit, :update, :destroy]
     resources :foods
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_213122) do
+ActiveRecord::Schema.define(version: 2021_04_10_115004) do
 
   create_table "foods", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 2021_04_09_213122) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "homes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
